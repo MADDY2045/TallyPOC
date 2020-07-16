948f1eddf3c48def9fcff0283b06dc0edf35ddf7
madhavan <madhavaneee08@gmail.com>
2020-07-16 10:09:11 +0530

    staged

--- server.js
+++ server.js
@@ -66,16 +66,161 @@ app.get('/',getGsApp,(req,res)=>{
                                                      }
                                                     })
                                                 })
-                                                console.log(JSON.stringify(tallyArrayObj,null,2));
-                                                res.render('pages/recon',{table:tallyArrayObj,table2:req.body});
+                                                // console.log(JSON.stringify(tallyArrayObj,null,2));
+                                                res.render('pages/recon',{table:tallyArrayObj,table2:req.body,message:false});
                                             })
                     }).catch(err=>console.log(err));
 
     }catch(error){console.log('error:::::::',error)}
 })
 
-app.get('/getrecondetails/:id',(req,res)=>{
+app.get('/getrecondetails/:id',getGsApp,(req,res)=>{
     console.log('entered',req.params.id);
+    let searchvoucher = req.params.id;
+    if(searchvoucher === 'All'){
+        try{
+            axios({url:'http://localhost:9000',method:'POST',headers:{ContentType: 'text/xml',charset:'UTF-8'},data:xmlstring})
+            .then(response=>{
+                            parseString(response.data, (err, result)=>{
+                                let tempArr=[];
+                                let tempObj ={};
+
+                                result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE.pop();
+                                 let exportData = result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE;
+                                                 exportData.map(item=>{
+                                                     if(item.VOUCHER[0].ISCANCELLED !='Yes'){
+                                                         let tempObj={};
+                                                         tempObj['tallyid']=item.VOUCHER[0].MASTERID[0].trim();
+                                                         tempObj['vchnumber']=item.VOUCHER[0].VOUCHERNUMBER[0];
+                                                         tempObj['vouchertype']=item.VOUCHER[0].$.VCHTYPE;
+                                                         tempObj['date']=item.VOUCHER[0].DATE[0];
+                                                        //   tempObj['items']=[];
+                                                        //   tempObj['items'].push(item.VOUCHER[0]);
+                                                        if(item.VOUCHER[0]['ALLINVENTORYENTRIES.LIST']){
+                                                            tempObj['amount']=item.VOUCHER[0]['ALLINVENTORYENTRIES.LIST'][0].AMOUNT[0];
+                                                        }else if(item.VOUCHER[0]['ALLLEDGERENTRIES.LIST']){
+                                                            tempObj['amount']=item.VOUCHER[0]['ALLLEDGERENTRIES.LIST'][0].AMOUNT[0];
+                                                        }else{
+                                                            tempObj['amount']=item.VOUCHER[0]['INVENTORYENTRIESIN.LIST'][0].AMOUNT[0];
+                                                        }
+
+                                                         tempArr.push(tempObj);
+                                                     }
+
+                                                 })
+                                                 let tallyArrayObj=[
+                                                     {'Sales':[],amount:0},
+                                                     {'Purchase':[],amount:0},
+                                                     {'Delivery Note':[],amount:0},
+                                                     {'Receipt Note':[],amount:0},
+                                                     {'Journal':[],amount:0},
+                                                     {'Receipt':[],amount:0},
+                                                     {'Payment':[],amount:0},
+                                                     {'Credit Note':[],amount:0},
+                                                     {'Debit Note':[],amount:0},
+                                                     {'Stock Journal':[],amount:0}
+                                                    ];
+                                                 tempArr.map(item=>{
+                                                     tallyArrayObj.map(obj=>{
+                                                         let keys=Object.keys(obj);
+
+                                                         if(item.vouchertype==keys[0]){
+                                                            obj[keys[1]] += Number(item.amount);
+                                                            obj[keys[0]].push(item);
+                                                         }
+                                                        })
+                                                    })
+                                                    // console.log(JSON.stringify(tallyArrayObj,null,2));
+                                                    res.render('pages/recon',{table:tallyArrayObj,table2:req.body,message:true,vouchername:null});
+                                                })
+                        }).catch(err=>console.log(err));
+
+        }catch(error){console.log('error:::::::',error)}
+    }else{
+        try{
+            console.log('searchvoucher is ::',searchvoucher);
+            axios({url:'http://localhost:9000',method:'POST',headers:{ContentType: 'text/xml',charset:'UTF-8'},data:xmlstring})
+            .then(response=>{
+                            parseString(response.data, (err, result)=>{
+                                let tempArr=[];
+                                let tempObj ={};
+
+                                result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE.pop();
+                                 let exportData = result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE;
+                                                 exportData.map(item=>{
+                                                     if(item.VOUCHER[0].ISCANCELLED !='Yes' && item.VOUCHER[0].$.VCHTYPE==searchvoucher){
+                                                         let tempObj={};
+                                                         tempObj['tallyid']=item.VOUCHER[0].MASTERID[0].trim();
+                                                         tempObj['vchnumber']=item.VOUCHER[0].VOUCHERNUMBER[0];
+                                                         tempObj['vouchertype']=item.VOUCHER[0].$.VCHTYPE;
+                                                         tempObj['date']=item.VOUCHER[0].DATE[0];
+                                                        //   tempObj['items']=[];
+                                                        //   tempObj['items'].push(item.VOUCHER[0]);
+                                                        if(item.VOUCHER[0]['ALLINVENTORYENTRIES.LIST']){
+                                                            tempObj['amount']=item.VOUCHER[0]['ALLINVENTORYENTRIES.LIST'][0].AMOUNT[0];
+                                                        }else if(item.VOUCHER[0]['ALLLEDGERENTRIES.LIST']){
+                                                            tempObj['amount']=item.VOUCHER[0]['ALLLEDGERENTRIES.LIST'][0].AMOUNT[0];
+                                                        }else{
+                                                            tempObj['amount']=item.VOUCHER[0]['INVENTORYENTRIESIN.LIST'][0].AMOUNT[0];
+                                                        }
+
+                                                         tempArr.push(tempObj);
+                                                     }
+
+                                                 })
+                                                 let tallyArrayObj=[
+                                                     {'Sales':[],amount:0},
+                                                     {'Purchase':[],amount:0},
+                                                     {'Delivery Note':[],amount:0},
+                                                     {'Receipt Note':[],amount:0},
+                                                     {'Journal':[],amount:0},
+                                                     {'Receipt':[],amount:0},
+                                                     {'Payment':[],amount:0},
+                                                     {'Credit Note':[],amount:0},
+                                                     {'Debit Note':[],amount:0},
+                                                     {'Stock Journal':[],amount:0}
+                                                    ];
+                                                    let tallyAppArr=[];
+                                                 tempArr.map(item=>{
+                                                     tallyArrayObj.map(obj=>{
+                                                         let keys=Object.keys(obj);
+
+                                                         if(item.vouchertype==keys[0]&&keys[0]==searchvoucher){
+                                                            obj[keys[1]] += Number(item.amount);
+                                                            obj[keys[0]].push(item);
+                                                         }
+                                                        })
+                                                    })
+                                                    tallyArrayObj.map(item=>{
+                                                        Object.keys(item).map(key=>{
+                                                            if(key==searchvoucher){
+                                                                console.log('keys are',key);
+                                                                tallyAppArr.push(item)
+                                                            }
+                                                        })
+
+
+                                                    })
+                                                    // console.log(JSON.stringify(tallyArrayObj,null,2));
+                                                    let GsAppArray = [];
+                                                    req.body.map(item=>{
+                                                        Object.keys(item).map(key=>{
+                                                            if(key==searchvoucher){
+                                                                console.log('keys are',key);
+                                                                GsAppArray.push(item)
+                                                            }
+                                                        })
+
+
+                                                    })
+                                                    //res.send(tallyAppArr);
+                                                    res.render('pages/recon',{table:tallyAppArr,table2:GsAppArray,message:true,vouchername:searchvoucher});
+                                                })
+                        }).catch(err=>console.log(err));
+
+        }catch(error){console.log('error:::::::',error)}
+
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
